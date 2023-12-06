b4b1dad7dc9cb76fc0b2bd81ad870035d2f676ff
boralex <boris.alexandre.rose@gmail.com>
2023-12-06 13:48:07 +0100

    feat/J3/users

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,10 +2,10 @@
 module.exports = (req, res, next) => {
 
    const { authorization } = req.headers
-   const token = authorization.split('')[1];
-   console.log('authorization', authorization)
-   
-
+   if(authorization.length){
+      const token = authorization.split('')[1];
+      console.log('authorization', authorization)
+   }
    next()
 
 
